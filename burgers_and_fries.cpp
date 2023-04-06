819d7b5bba30eaecdfc087997ab4429241df473a
texjack19 <texjack19@gmail.com>
2023-04-06 22:26:25 +0000

    Finished Exam 2.2b

--- burgers_and_fries.cpp
+++ burgers_and_fries.cpp
@@ -13,6 +13,17 @@ const char* type_names[] = {"BURGER", "FRIES"};
 
 int k;
 
+// create semaphores for burger and fries, mutex
+sem_t burger_sem;
+sem_t fries_sem;
+mutex mtx;
+
+// vars to count/keep track of orders
+int waitBurger = 0;
+int waitFries = 0;
+int in_progress_burgers = 0;
+int in_progress_fries = 0;
+
 // Do not change
 void process_order() {
     sleep(2);
@@ -26,6 +37,43 @@ void place_order(int type) {
      *     otherwise place this order (print order)
      *  Use type_names[type] to print the order type
      */
+    
+    // lock
+    mtx.lock();
+
+    // if k orders are already in process, wait
+    if ((type == BURGER && in_progress_burgers >= k) || (type == FRIES && in_progress_fries >= k)) {
+        if (type == BURGER) {
+            waitBurger++;
+            cout << "Waiting: " << type_names[type] << endl;
+            mtx.unlock();
+            sem_wait(&burger_sem);
+        }
+        // case for fries order
+        else {
+            waitFries++;
+            cout << "Waiting: " << type_names[type] << endl;
+            mtx.unlock();
+            sem_wait(&fries_sem);
+        }
+    }
+    // otherwise, place the order
+    else {
+        // if order is burger
+        if (type == BURGER) {
+            in_progress_burgers++;
+            cout << "Order: " << type_names[type] << endl;
+            mtx.unlock();
+            sem_post(&burger_sem);
+        } 
+        else {
+            in_progress_fries++;
+            cout << "Order: " << type_names[type] << endl;
+            mtx.unlock();
+            sem_post(&fries_sem);
+        }
+    }
+
 
     process_order();        // Do not remove, simulates preparation
 
@@ -33,6 +81,48 @@ void place_order(int type) {
      *  Add logic for synchronization after order processed
      *  Allow next order of the same type to proceed if there is any waiting; if not, allow the other type to proceed.
      */
+
+    // lock
+    mtx.lock();
+
+    // if waiting, allow order of the same type to proceed
+    if (type == BURGER) {
+        in_progress_burgers--;
+        if (waitBurger > 0) {
+            waitBurger--;
+            cout << "Order: " << type_names[type] << endl;
+            mtx.unlock();
+            sem_post(&burger_sem);
+        } 
+        else if (waitFries > 0) {
+            in_progress_fries--;
+            cout << "Order: " << type_names[type] << endl;
+            mtx.unlock();
+            sem_post(&fries_sem);
+        } 
+        else {
+            mtx.unlock();
+        }
+    }
+    // if fries order
+    else {
+        in_progress_fries--;
+        if (waitFries > 0) {
+            waitFries--;
+            cout << "Order: " << type_names[type] << endl;
+            mtx.unlock();
+            sem_post(&fries_sem);
+        } 
+        else if (waitBurger > 0) {
+            in_progress_burgers--;
+            cout << "Order: " << type_names[type] << endl;
+            mtx.unlock();
+            sem_post(&burger_sem);
+        } 
+        else {
+            mtx.unlock();
+        }
+    }
 }
 
 int main() {
